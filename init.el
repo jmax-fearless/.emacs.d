ea4382f534e531734eb2c32839455a92e7cd23e0
John Maxwell <jmax@jmaxhome.com>
2017-03-29 10:12:15 -0400

    Set font

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Bitstream Vera Sans Mono" :foundry "bitstream" :slant normal :weight normal :height 108 :width normal)))))
 
 (put 'narrow-to-region 'disabled nil)
 
